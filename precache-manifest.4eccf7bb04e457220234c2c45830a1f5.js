47370fc7103e7aad5015f7fe8185510e3fe9961f
Adam Alvis <adam.alvis@gmail.com>
2020-07-15 10:43:08 -0400

    Updates

---
+++ precache-manifest.4eccf7bb04e457220234c2c45830a1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91e1a575b97a490ccca6c3b2d145d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7eee3acedc07504c43",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "075e74267be33074be47",
+    "url": "/static/js/2.aa884c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa884c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7eee3acedc07504c43",
+    "url": "/static/js/main.61b46b93.chunk.js"
+  },
+  {
+    "revision": "6ab04c2e6bec049a3974",
+    "url": "/static/js/runtime-main.a3350441.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
